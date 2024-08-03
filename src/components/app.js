26e42350bc37989ff9cf4b5af611e17bac0bac71
Goa-Zen <goazengoazen.cr@gmail.com>
2024-08-03 21:35:11 +0200

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from "./portfolio-container";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        
+        <h1>Silvia Changed React Redux Router</h1>
+
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+
+        <PortfolioContainer />
+
+      </div>
+    );
+  }
+}
