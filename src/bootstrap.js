44b23917b73f1e8a992ed5628d6d819fc8de8f4c
Goa-Zen <goazengoazen.cr@gmail.com>
2024-09-14 19:14:16 +0200

    another commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
-// import 'leaflet/dist/leaflet.css';
+
 import App from "./components/app";
 import reducers from "./reducers";
 
